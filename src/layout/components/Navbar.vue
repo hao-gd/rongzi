0678d5751f7fe5c27f582ba2f7d07162ec8211bd
hao-gd <3145534035@qq.com>
2024-03-25 22:38:04 +0800

    1

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -91,7 +91,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index';
+          location.href = '/';
         })
       }).catch(() => {});
     }
