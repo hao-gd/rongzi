7293b0e728237ef8830c160b968a5eaf756ba7ab
honolulu0 <1037494014@qq.com/>
2024-04-15 15:05:12 +0800

    提款信息输入区，本金偿还信息输入区，年利率信息输入区，四舍五入保留2位小数

--- src/views/financingproject/project/form.js
+++ src/views/financingproject/project/form.js
@@ -1,252 +1,341 @@
-import { Numeric as TinyNumeric } from '@opentiny/vue'
+import {
+  Numeric as TinyNumeric
+} from '@opentiny/vue'
 // import { ElDatePicker } from 'element-ui'
 
 export const resetForm = {
-    id: null,
-    managementId: null,
-    scrUuid: null,
-    borrowingUnit: null,
-    financialInstitution: null,
-    financingAmount: null,
-    financingType: null,
-    contractId: null,
-    contractSigningDate: null,
-    loanDate: null,
-    dueDate: null,
-    rate: null,
-    loanTerm: null,
-    creditEnhancementMeasures: null,
-    repaidAmount: null,
-    remainingAmount: null,
-    loanState: null,
-    comment: null,
-    createTime: null,
-    createBy: null,
-    updateTime: null,
-    updateBy: null,
-    uuid: null,
-    principalRepaymentMethod: null,
-    interestRepaymentMethod: null,
-    rateType: null,
-    firstRepaymentDate: null,
-    hasRepaymentPlan: null,
-    tiqubenjin: null,
-    changhuanbenjin: null,
-    lilvbiangeng: null,
-    lixichanghuan: null,
-    huankuanmingxi2List: null,
-    baozhengjin: null,
-    shouxufei: null,
+  id: null,
+  managementId: null,
+  scrUuid: null,
+  borrowingUnit: null,
+  financialInstitution: null,
+  financingAmount: null,
+  financingType: null,
+  contractId: null,
+  contractSigningDate: null,
+  loanDate: null,
+  dueDate: null,
+  rate: null,
+  loanTerm: null,
+  creditEnhancementMeasures: null,
+  repaidAmount: null,
+  remainingAmount: null,
+  loanState: null,
+  comment: null,
+  createTime: null,
+  createBy: null,
+  updateTime: null,
+  updateBy: null,
+  uuid: null,
+  principalRepaymentMethod: null,
+  interestRepaymentMethod: null,
+  rateType: null,
+  firstRepaymentDate: null,
+  hasRepaymentPlan: null,
+  tiqubenjin: null,
+  changhuanbenjin: null,
+  lilvbiangeng: null,
+  lixichanghuan: null,
+  huankuanmingxi2List: null,
+  baozhengjin: null,
+  shouxufei: null,
 }
 
 export const queryParams = {
-    pageNum: 1,
-    pageSize: 100,
-    managementId: null,
-    scrUuid: null,
-    borrowingUnit: null,
-    financialInstitution: null,
-    financingAmount: null,
-    financingType: null,
-    contractId: null,
-    contractSigningDate: null,
-    loanDate: null,
-    dueDate: null,
-    rate: null,
-    loanTerm: null,
-    creditEnhancementMeasures: null,
-    repaidAmount: null,
-    remainingAmount: null,
-    loanState: null,
-    comment: null,
-    createBy: null,
-    updateBy: null,
-    uuid: null,
-    principalRepaymentMethod: null,
-    interestRepaymentMethod: null,
-    rateType: null,
-    firstRepaymentDate: null,
-    hasRepaymentPlan: null,
-    baozhengjin: null,
-    shouxufei: null,
+  pageNum: 1,
+  pageSize: 100,
+  managementId: null,
+  scrUuid: null,
+  borrowingUnit: null,
+  financialInstitution: null,
+  financingAmount: null,
+  financingType: null,
+  contractId: null,
+  contractSigningDate: null,
+  loanDate: null,
+  dueDate: null,
+  rate: null,
+  loanTerm: null,
+  creditEnhancementMeasures: null,
+  repaidAmount: null,
+  remainingAmount: null,
+  loanState: null,
+  comment: null,
+  createBy: null,
+  updateBy: null,
+  uuid: null,
+  principalRepaymentMethod: null,
+  interestRepaymentMethod: null,
+  rateType: null,
+  firstRepaymentDate: null,
+  hasRepaymentPlan: null,
+  baozhengjin: null,
+  shouxufei: null,
 }
 
 export const rules = {
-    managementId: [
-        { required: true, message: "贷后项目管理编号不能为空", trigger: "blur" }
-    ],
-    scrUuid: [
-        { required: true, message: "数据唯一编号不能为空", trigger: "blur" }
-    ],
-    borrowingUnit: [
-        { required: true, message: "借款人不能为空", trigger: "change" }
-    ],
-    financialInstitution: [
-        { required: true, message: "债权人不能为空", trigger: "change" }
-    ],
-    financingAmount: [
-        { required: true, message: "融资金额不能为空", trigger: "blur" }
-    ],
-    financingType: [
-        { required: true, message: "融资类型不能为空", trigger: "change" }
-    ],
-    contractId: [
-        { required: true, message: "资金用途不能为空", trigger: "blur" }
-    ],
-    contractSigningDate: [
-        { required: true, message: "合同签订日期不能为空", trigger: "blur" }
-    ],
-    loanDate: [
-        { required: true, message: "放款日不能为空", trigger: "blur" }
-    ],
-    dueDate: [
-        { required: true, message: "到期日不能为空", trigger: "blur" }
-    ],
-    rate: [
-        { required: false, message: "年利率不能为空", trigger: "blur" }
-    ],
-    loanTerm: [
-        { required: true, message: "借款期限不能为空", trigger: "blur" }
-    ],
-    creditEnhancementMeasures: [
-        { required: true, message: "增信措施不能为空", trigger: "blur" }
-    ],
-    repaidAmount: [
-        { required: true, message: "已还金额不能为空", trigger: "blur" }
-    ],
-    remainingAmount: [
-        { required: true, message: "融资余额不能为空", trigger: "blur" }
-    ],
-    loanState: [
-        { required: true, message: "债务状态不能为空", trigger: "change" }
-    ],
-    principalRepaymentMethod: [
-        { required: true, message: "本金偿还方式不能为空", trigger: "change" }
-    ],
-    interestRepaymentMethod: [
-        { required: true, message: "利息偿还方式不能为空", trigger: "change" }
-    ],
-    rateType: [
-        { required: true, message: "是否固定利率不能为空", trigger: "change" }
-    ],
-    firstRepaymentDate: [
-        { required: true, message: "第1期开始时间不能为空", trigger: "blur" }
-    ],
-    hasRepaymentPlan: [
-        { required: true, message: "是否有还款计划不能为空", trigger: "change" }
-    ],
-    tiqubenjin: [
-        { required: true, message: "提取本金不能为空", trigger: "blur" }
-    ],
-    changhuanbenjin: [
-        { required: true, message: "偿还本金不能为空", trigger: "blur" }
-    ],
-    lilvbiangeng: [
-        { required: true, message: "利率变更不能为空", trigger: "blur" }
-    ],
-    lixichanghuan: [
-        { required: true, message: "利息偿还不能为空", trigger: "blur" }
-    ],
-    baozhengjin: [
-        { required: true, message: "保证金不能为空", trigger: "blur" }
-      ],
-      shouxufei: [
-        { required: true, message: "手续费不能为空", trigger: "blur" }
-      ],
+  managementId: [{
+    required: true,
+    message: "贷后项目管理编号不能为空",
+    trigger: "blur"
+  }],
+  scrUuid: [{
+    required: true,
+    message: "数据唯一编号不能为空",
+    trigger: "blur"
+  }],
+  borrowingUnit: [{
+    required: true,
+    message: "借款人不能为空",
+    trigger: "change"
+  }],
+  financialInstitution: [{
+    required: true,
+    message: "债权人不能为空",
+    trigger: "change"
+  }],
+  financingAmount: [{
+    required: true,
+    message: "融资金额不能为空",
+    trigger: "blur"
+  }],
+  financingType: [{
+    required: true,
+    message: "融资类型不能为空",
+    trigger: "change"
+  }],
+  contractId: [{
+    required: true,
+    message: "资金用途不能为空",
+    trigger: "blur"
+  }],
+  contractSigningDate: [{
+    required: true,
+    message: "合同签订日期不能为空",
+    trigger: "blur"
+  }],
+  loanDate: [{
+    required: true,
+    message: "放款日不能为空",
+    trigger: "blur"
+  }],
+  dueDate: [{
+    required: true,
+    message: "到期日不能为空",
+    trigger: "blur"
+  }],
+  rate: [{
+    required: false,
+    message: "年利率不能为空",
+    trigger: "blur"
+  }],
+  loanTerm: [{
+    required: true,
+    message: "借款期限不能为空",
+    trigger: "blur"
+  }],
+  creditEnhancementMeasures: [{
+    required: true,
+    message: "增信措施不能为空",
+    trigger: "blur"
+  }],
+  repaidAmount: [{
+    required: true,
+    message: "已还金额不能为空",
+    trigger: "blur"
+  }],
+  remainingAmount: [{
+    required: true,
+    message: "融资余额不能为空",
+    trigger: "blur"
+  }],
+  loanState: [{
+    required: true,
+    message: "债务状态不能为空",
+    trigger: "change"
+  }],
+  principalRepaymentMethod: [{
+    required: true,
+    message: "本金偿还方式不能为空",
+    trigger: "change"
+  }],
+  interestRepaymentMethod: [{
+    required: true,
+    message: "利息偿还方式不能为空",
+    trigger: "change"
+  }],
+  rateType: [{
+    required: true,
+    message: "是否固定利率不能为空",
+    trigger: "change"
+  }],
+  firstRepaymentDate: [{
+    required: true,
+    message: "第1期开始时间不能为空",
+    trigger: "blur"
+  }],
+  hasRepaymentPlan: [{
+    required: true,
+    message: "是否有还款计划不能为空",
+    trigger: "change"
+  }],
+  tiqubenjin: [{
+    required: true,
+    message: "提取本金不能为空",
+    trigger: "blur"
+  }],
+  changhuanbenjin: [{
+    required: true,
+    message: "偿还本金不能为空",
+    trigger: "blur"
+  }],
+  lilvbiangeng: [{
+    required: true,
+    message: "利率变更不能为空",
+    trigger: "blur"
+  }],
+  lixichanghuan: [{
+    required: true,
+    message: "利息偿还不能为空",
+    trigger: "blur"
+  }],
+  baozhengjin: [{
+    required: true,
+    message: "保证金不能为空",
+    trigger: "blur"
+  }],
+  shouxufei: [{
+    required: true,
+    message: "手续费不能为空",
+    trigger: "blur"
+  }],
 }
 
 // 生成 还款计划的edit 组件
 export function renderInput(field) {
-    return (h, { row }) =>
-        row.editing
-            ? h('tiny-numeric', {
-                class: 'w',
-                props: { modelValue: Number(row[field]), size: 'small', "show-left": false, controls: false, precision: 2, format: {fraction: 2, rounding: 0, zeroize: true,fractionGroupSize: 0, } },
-                on: { 'change': (value) => (row[field] = Number(value)) }
-            })
-            : h('span', row[field])
+  return (h, {
+      row
+    }) =>
+    row.editing ?
+    h('tiny-numeric', {
+      class: 'w',
+      props: {
+        modelValue: Number(row[field]),
+        size: 'small',
+        "show-left": false,
+        controls: false,
+        precision: 2,
+      },
+      on: {
+        'change': (value) => (row[field] = Number(value.toFixed(2)))
+      }
+    }) :
+    h('span', row[field])
 }
 // type: 'number', controls: false, precision: 2
 export function renderRate(field) {
-    return (h, { row }) =>
-        row.editing
-            ? h('tiny-numeric', {
-                class: 'w',
-                props: { modelValue: Number(row[field]), size: 'small', "show-left": false, controls: false, precision: 2, format: { suffix: '%', fraction: 2, rounding: 0, zeroize: true,fractionGroupSize: 0, } },
-                on: { 'change': (value) => (row[field] = Number(value)) }
-            })
-            : h('span', row[field])
+  return (h, {
+      row
+    }) =>
+    row.editing ?
+    h('tiny-numeric', {
+      class: 'w',
+      props: {
+        modelValue: Number(row[field]),
+        size: 'small',
+        "show-left": false,
+        controls: false,
+        precision: 2,
+        format: {
+          suffix: '%',
+        }
+      },
+      on: {
+        'change': (value) => (row[field] = Number(value.toFixed(2)))
+      }
+    }) :
+    h('span', row[field])
 }
 export function renderDate(field) {
-    return (h, { row }) =>
-        row.editing
-            ? h("el-date-picker", {
-                props: { value: row[field], format: 'yyyy/MM/dd', valueFormat: 'yyyy-MM-dd', type: "date" },
-                on: { 'input': (value) => (row[field] = value) }
-            })
-            : h('span', row[field])
+  return (h, {
+      row
+    }) =>
+    row.editing ?
+    h("el-date-picker", {
+      props: {
+        value: row[field],
+        format: 'yyyy/MM/dd',
+        valueFormat: 'yyyy-MM-dd',
+        type: "date"
+      },
+      on: {
+        'input': (value) => (row[field] = value)
+      }
+    }) :
+    h('span', row[field])
 }
-export const hkjh_repaymentPlanClearingTableColumn = [
-    {
-        label: '期数',
-        prop: 'qishu',
-        width: '',
-        minWidth: '100',
-    },
-    {
-        label: '日期',
-        prop: 'riqi',
-        width: '',
-        minWidth: '100',
-    },
-    {
-        label: '还款金额',
-        prop: 'huankuanjine',
-        width: '',
-        minWidth: '100',
-    },
-    {
-        label: '偿还本金',
-        prop: 'changhuanben',
-        width: '',
-        minWidth: '100',
-    },
-    {
-        label: '支付利息',
-        prop: 'zhifulixi',
-        width: '',
-        minWidth: '100',
-    },
-    {
-        label: '本金剩余',
-        prop: 'benjinshengyu',
-        width: '',
-        minWidth: '100',
-    },
-    // {
-    //     label: '利率',
-    //     prop: 'lilv',
-    //     width: '',
-    //     minWidth: '100',
-    // },
-    // {
-    //     label: '备注',
-    //     prop: '备注',
-    //     width: '',
-    //     minWidth: '100',
-    // }
+export const hkjh_repaymentPlanClearingTableColumn = [{
+    label: '期数',
+    prop: 'qishu',
+    width: '',
+    minWidth: '100',
+  },
+  {
+    label: '日期',
+    prop: 'riqi',
+    width: '',
+    minWidth: '100',
+  },
+  {
+    label: '还款金额',
+    prop: 'huankuanjine',
+    width: '',
+    minWidth: '100',
+  },
+  {
+    label: '偿还本金',
+    prop: 'changhuanben',
+    width: '',
+    minWidth: '100',
+  },
+  {
+    label: '支付利息',
+    prop: 'zhifulixi',
+    width: '',
+    minWidth: '100',
+  },
+  {
+    label: '本金剩余',
+    prop: 'benjinshengyu',
+    width: '',
+    minWidth: '100',
+  },
+  // {
+  //     label: '利率',
+  //     prop: 'lilv',
+  //     width: '',
+  //     minWidth: '100',
+  // },
+  // {
+  //     label: '备注',
+  //     prop: '备注',
+  //     width: '',
+  //     minWidth: '100',
+  // }
 ]
 
 export function multiplySelectedFields(list, multiplier, fields) {
-    return list.map(item => {
-        let newItem = { ...item };
-        fields.forEach(field => {
-            if (newItem.hasOwnProperty(field)) {
-                newItem[field] = newItem[field] * multiplier;
-            }
-        });
-        return newItem;
+  return list.map(item => {
+    let newItem = {
+      ...item
+    };
+    fields.forEach(field => {
+      if (newItem.hasOwnProperty(field)) {
+        newItem[field] = newItem[field] * multiplier;
+      }
     });
+    return newItem;
+  });
 }
 
 
